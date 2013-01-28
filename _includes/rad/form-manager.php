17b833382453c360757b8bdb3e589d7264450ebf
Konstantin Kudryashov <ever.zet@gmail.com>
2013-01-28 23:07:54 +0100

    5.4ize and fix

--- _includes/rad/form-manager.php
+++ _includes/rad/form-manager.php
@@ -1,19 +1,17 @@
 <?php
 
-/**
- * You don't even have to create a form type!
- */
 public function editAction()
 {
+    // You don't even have to create a form type!
+    // Form will also automatically be bound, but
+    // only on non-safe request (POST|PUT|DELETE)
     $form = $this->createBoundObjectForm(new BlogPost, 'edit');
 
     if ($form->isBound() && $form->isValid()) {
         // do some stuffs with $blogPost
 
-        return $this->redirectToUrl('app_blogpost_index');
+        return $this->redirectToRoute('app_blogpost_index');
     }
 
-    return array(
-        'form' => $form->createView(),
-    );
+    return ['form' => $form->createView()];
 }
