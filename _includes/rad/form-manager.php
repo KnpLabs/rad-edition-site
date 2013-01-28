bb86952e35d57a0aed6e9ca0a208b6643b9a2322
Konstantin Kudryashov <ever.zet@gmail.com>
2013-01-28 23:24:52 +0100

    Removed comments & added more real code
    Goal of this example is to show how clean/short it is.
    Not to explain it's extensibility - that's what wiki is for.

--- _includes/rad/form-manager.php
+++ _includes/rad/form-manager.php
@@ -2,13 +2,11 @@
 
 public function editAction()
 {
-    // You don't even have to create a form type!
-    // Form will also automatically be bound, but
-    // only on non-safe request (POST|PUT|DELETE)
     $form = $this->createBoundObjectForm(new BlogPost, 'edit');
 
     if ($form->isBound() && $form->isValid()) {
-        // do some stuffs with $blogPost
+        $this->flush();
+        $this->addFlash('info');
 
         return $this->redirectToRoute('app_blogpost_index');
     }
