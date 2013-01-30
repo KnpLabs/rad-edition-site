6c9549463f3be669485f2223c9f6bb29aa09cf11
Gildas Quéméner <gildas.quemener@knplabs.com>
2013-01-30 14:39:00 +0100

    Added contributors' avatar

---
+++ javascript/rad.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://search.twitter.com/search.json',
+        data: {
+            q: 'KnpRadBundle',
+            result_type: 'recent',
+            rpp: 5
+        },
+        success: function(data) {
+            var tweets = [];
+            data.results.forEach(function(tweet) {
+                tweets.push('<b>' + tweet.from_user_name + '</b> ' + tweet.text);
+            });
+            tweets = '<ul><li>' + tweets.join('</li><li>') + '</li></ul>';
+            $('#tweets').html(replaceURLWithHTMLLinks(tweets));
+        },
+        dataType: 'jsonp',
+        cache: true
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.github.com/repos/KnpLabs/KnpRadBundle/contributors',
+        success: function(data) {
+            console.log(data);
+            var contributors = [];
+            data.forEach(function(contributor) {
+                contributors.push('<a href="https://github.com/' + contributor.login + '"><img src="' + contributor.avatar_url + '" title="' + contributor.login + '" /></a>');
+            });
+            $('#contributors').html(contributors);
+        }
+    });
+
+    function replaceURLWithHTMLLinks(text) {
+        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
+        return text.replace(exp,"<a href='$1'>$1</a>");
+    }
+});
