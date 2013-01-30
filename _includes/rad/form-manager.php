bd4827079ee606fa29d97104fe8062fa2166cff8
Konstantin Kudryashov <ever.zet@gmail.com>
2013-01-30 11:04:53 +0100

    Fix example

--- _includes/rad/form-manager.php
+++ _includes/rad/form-manager.php
@@ -1,14 +1,14 @@
 <?php
 
-public function editAction()
+public function newAction()
 {
-    $form = $this->createBoundObjectForm(new BlogPost, 'edit');
+    $form = $this->createBoundObjectForm(new BlogPost, 'new');
 
     if ($form->isBound() && $form->isValid()) {
         $this->flush();
         $this->addFlash('success');
 
-        return $this->redirectToRoute('app_blogpost_index');
+        return $this->redirectToRoute('app_blogposts_index');
     }
 
     return ['form' => $form->createView()];
