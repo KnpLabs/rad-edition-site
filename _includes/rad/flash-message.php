655e2f6b1e8a21a4e856a2b17d305c4ae0f4aa35
Konstantin Kudryashov <ever.zet@gmail.com>
2013-01-30 12:24:22 +0100

    Update _includes/rad/flash-message.php

--- _includes/rad/flash-message.php
+++ _includes/rad/flash-message.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App\Controller;
-
-use Knp\RadBundle\Controller\Controller;
-
-class DefaultController extends Controller
+// i18n-driven flash messages (actual)
+// messages are in messages.yml and key is
+// used in controllers - `app_blogposts_update.success`
+public function updateAction()
 {
-    public function updateAction()
-    {
-        $this->addFlash('success');
-    }
+    $this->addFlash('success');
+}
+
+// entire message in controller
+public function newAction()
+{
+    $this->addFlash('success', 'Successfully created');
 }
