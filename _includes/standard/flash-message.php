720e2a9fc3d9f1ba32b6295825db5cdee33df5e1
Konstantin Kudryashov <ever.zet@gmail.com>
2013-01-30 12:25:53 +0100

    Update _includes/standard/flash-message.php

--- _includes/standard/flash-message.php
+++ _includes/standard/flash-message.php
@@ -1,19 +1,21 @@
 <?php
 
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class DefaultController extends Controller
+// i18n-driven flash messages (actual)
+// messages are in messages.yml and key is
+// used in controllers - `app_blogposts_update.success`
+public function updateAction()
 {
-    public function updateAction()
-    {
-        $this
-            ->get('session')
-            ->getFlashBag()
-            ->add(
-                'success',
-                'Your modifications were successfully saved!'
-            );
-    }
+    $this->get('session')
+        ->getFlashBag()
+        ->add('success', 'app_blogposts_update.success')
+    ;
+}
+
+// entire message in controller
+public function newAction()
+{
+    $this->get('session')
+        ->getFlashBag()
+        ->add('success', 'Successfully created')
+    ;
 }
