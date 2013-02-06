42d148eb451db32d1357977d4204300a11716200
Klein Florian <florian.klein@free.fr>
2013-02-06 16:55:56 +0100

    fix typo

--- _includes/rad/unsafe-methods.php
+++ _includes/rad/unsafe-methods.php
@@ -2,7 +2,7 @@
 
 public function deleteAction($id)
 {
-    $this->findOr404($id);
+    $this->findOr404('App:BlogPost', ['id' => $id]);
     $this->remove($blogPost, true);
 
     return $this->redirectToRoute('app_blogposts_index');
