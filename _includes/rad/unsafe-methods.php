d282083f18a55d1ceb1960d326a185912ffd1d19
Aurélien FONTAINE <aurelien@attraktiv.fr>
2013-06-03 19:04:24 +0300

    Update unsafe-methods.php
    Missing blogPost var

--- _includes/rad/unsafe-methods.php
+++ _includes/rad/unsafe-methods.php
@@ -2,7 +2,7 @@
 
 public function deleteAction($id)
 {
-    $this->findOr404('App:BlogPost', ['id' => $id]);
+    $blogPost = $this->findOr404('App:BlogPost', ['id' => $id]);
     $this->remove($blogPost, true);
 
     return $this->redirectToRoute('app_blogposts_index');
